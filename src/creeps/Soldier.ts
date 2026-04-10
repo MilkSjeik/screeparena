3c8d9da5292a2a5255d1d623a1161916424b569f
Stijn Mertens <stijn.mertens@expertum.net>
2026-04-10 22:06:42 +0200

    feat: implemented lean version of close ranged creep soldier

--- src/creeps/Soldier.ts
+++ src/creeps/Soldier.ts
@@ -4,64 +4,30 @@ import {
   ATTACK,
   MOVE,
   ERR_NOT_IN_RANGE,
-  RESOURCE_ENERGY,
+  TOUGH,
 } from "game/constants";
-import { getRange } from "game/utils";
+import { Creep } from "game/prototypes";
 import BaseCreep from "./BaseCreep";
-// import { GUARD, SOLDIER } from "../constants";
+import { Role } from "../constants";
 import SpawnQueue from "../SpawnQueue";
+import BaseSquad from "squads/BaseSquad";
 
 class Soldier extends BaseCreep {
-  // // Private
-  // objective;
-  // task;
-  // moveDirection;
-  //
-  // /**
-  //  * Soldier creep: close combat
-  //  * @constructor
-  //  * @param {SpawnQueue} spawnQueue - My spawn location
-  //  * @param {Number} squad - Squad
-  //  * @param {Number} memberId - Member Id
-  //  */
-  // constructor(spawnQueue, squad, memberId) {
-  //   super(squad.squadId, memberId, SOLDIER);
-  //
-  //   this.body = [ATTACK, ATTACK, MOVE, MOVE];
-  //   this.queueSpawn(spawnQueue, squad);
-  // }
-  //
-  // // Methods
-  // /**
-  //  * Execute the default action for a solder creep: attack
-  //  */
-  // run() {
-  //   if (!this.task) {
-  //     console.log("[D] Soldier reporting for duty Sir!");
-  //   } else {
-  //     if (this.task == GUARD) {
-  //       console.log("[D] Soldier guarding our spawn.");
-  //       // If not in (close) range, move to objective
-  //       if (getRange(this.creep, this.objective) > 1) {
-  //         this.creep.move(this.moveDirection);
-  //       }
-  //     } else if (this.task == ATTACK) {
-  //       console.log(
-  //         "[D] Soldier attacking the objective: " +
-  //           JSON.stringify(this.objective) +
-  //           ". (Squad = " +
-  //           this.squadId +
-  //           "; Member = " +
-  //           this.memberId +
-  //           ")"
-  //       );
-  //       if (this.creep.attack(this.objective) == ERR_NOT_IN_RANGE) {
-  //         console.log("[D] Target not in range! Moving closer!");
-  //         this.creep.move(this.moveDirection);
-  //       }
-  //     }
-  //   }
-  // }
+  target: Creep | undefined;
+  isLeader = false;
+
+  constructor(spawnQueue: SpawnQueue, squad: BaseSquad, memberId: number) {
+    super(squad.id, memberId, Role.SOLDIER);
+    this.body = [TOUGH, TOUGH, ATTACK, MOVE];
+    this.queueSpawn(spawnQueue, squad);
+  }
+
+  run() {
+    if (!this.creep || !this.target) return;
+    if (this.creep.attack(this.target) == ERR_NOT_IN_RANGE) {
+      this.creep.moveTo(this.target);
+    }
+  }
 }
 
 export default Soldier;
